173deaa687abf17163f63336a7e838e7bbfafaf7
Justin Gordon <justin.gordon@gmail.com>
2020-06-13 14:14:15 -1000

    Clarify file is from rails/webpacker

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,3 +1,7 @@
+//********************************************************************************
+// This file is created by the rails/webpacker installer.
+// Not the React on rails one!
+//********************************************************************************
 // Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
